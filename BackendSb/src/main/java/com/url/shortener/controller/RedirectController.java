b440298b75c1ab9d3cb1fe40c93354317f84d4d5
Sky <126008711+sumitmishra01@users.noreply.github.com>
2025-06-17 17:34:56 +0530

    Refactor: folder structure

---
+++ BackendSb/src/main/java/com/url/shortener/controller/RedirectController.java
@@ -0,0 +1,39 @@
+package com.url.shortener.controller;
+
+import com.url.shortener.models.UrlMapping;
+import com.url.shortener.service.UrlMappingService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedirectController {
+
+    public RedirectController(UrlMappingService urlMappingService) {
+        this.urlMappingService = urlMappingService;
+    }
+
+    public UrlMappingService getUrlMappingService() {
+        return urlMappingService;
+    }
+
+    public void setUrlMappingService(UrlMappingService urlMappingService) {
+        this.urlMappingService = urlMappingService;
+    }
+
+    private UrlMappingService urlMappingService;
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortUrl){
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
+        if (urlMapping != null) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Location", urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(httpHeaders).build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
