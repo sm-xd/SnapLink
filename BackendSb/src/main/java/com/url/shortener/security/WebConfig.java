9b0145f1e7687f9c45bf56e1f02c18695c415441
Sky <126008711+sumitmishra01@users.noreply.github.com>
2025-06-19 17:34:56 +0530

    fix: minor changes

--- BackendSb/src/main/java/com/url/shortener/security/WebConfig.java
+++ BackendSb/src/main/java/com/url/shortener/security/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${frontend.url")
+    @Value("${frontend.url}")
     String frontEndUrl;
 
     @Override
